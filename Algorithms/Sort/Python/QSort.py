58932020f80d7c4f899101849898cc192d20841e
MASL <miguel.a.santos.l@gmail.com>
2018-04-11 10:08:23 -0400

    Added Idea and non-recursive

--- Algorithms/Sort/Python/QSort.py
+++ Algorithms/Sort/Python/QSort.py
@@ -1,21 +1,21 @@
 
 def Qsort(A,l=0,r=-1):
 	"""
-	Recursively partition by pivot left and right blocks.
+	Recursively partition by pivot into left and right blocks.
 	"""
 	if r == -1 and l == 0 : r = len(A)-1
-	#print("DEBUG: l="+str(l)+" r="+str(r)+"  A="+str(A), end="\t" )
+	print("DEBUG: QS Bef. P: l="+str(l)+" r="+str(r)+"  A="+str(A), end="\t" )
 	if r > l:
-		p = partition(A,l, r)
-		#print("DEBUG: p="+str(p)+" A["+str(p)+"]="+str(A[p]) )
+		p = partitionRec(A,l, r)
+		print("DEBUG: QS Aft. P: p="+str(p)+" A["+str(p)+"]="+str(A[p]) )
 		Qsort(A,l,p-1)
 		Qsort(A,p+1,r)
 
 def partition(A,l,r):
 	"""
-	Idea:
-	Partition sorting by pivot array A between l-th and r-th elements (both included).
-	That is, once done, all element < pivot, will be on its left; all > pivot, on its right.
+	Idea:  Partition by pivot
+	Partition array A between l-th and r-th elements (both included) using a given value as the "pivot".
+	That is, once done, all elements < pivot, will be on left of pivot; all > pivot, on its right.
 
 	Choose as pivot A[p] with p=0, i.e., the first element of array.
 
@@ -24,6 +24,15 @@ def partition(A,l,r):
 	Otherwise, swap A[i] w/ A[p+1], then swap A[p] w/ A[p+1]. Effectively, this will put
 	the original A[i] on the left of the pivot A[p], which has moved one to the right to
 	accommodate A[i]. The original value on the right of A[p] is moved to where A[i] was.
+
+                                        If  A[i] <= A[p] then
+
+	Initiallly:        A[0] ... A[p-1] A[p] A[p+1] ... A[i]   ... A[N-1]
+                                             ^              ^^
+	After 1st swap:    A[0] ... A[p-1] A[p] A[i]  ...  A[p+1] ... A[N-1]
+                                             ^   ^^
+	After 2nd swap:    A[0] ... A[p-1] A[i] A[p]  ...  A[p+1] ... A[N-1]
+                                            ^^    ^
 
 	Increment p by 1.
 	Repeat for all i from p+1 to r. 
@@ -59,9 +68,13 @@ def partitionRec(A,l,r):
 
 	Whence, we must unfold this recursive version into one using a well-determined loop over the array.
 	"""
+	print("DEBUG: P-range l:"+str(l)+" r:"+str(r))
+	if r <= l : return r+1
+
 	p = partitionRec(A,l,r-1)
 	# p == r-1 + 1  if no swap was done
 
+	print("DEBUG: swapping p:"+str(p)+" r:"+str(r))
 	#new pivot
 	np = r+1
 	if A[p] < A[r] or p == r : return np
@@ -78,14 +91,17 @@ def partitionRec(A,l,r):
 	return np
 
 #r = [87, -3, 1, 6, -17, 3, 2, 1,7]
+#r = [87, -3,1,6]
+r = [1,6]
 #r = [87, -3, 1, 6, -17, 3, 2, 7]
 #r = [2,2,2,2,2,2]
 #r = r +r
 
+"""
 from random import random
 import math
 
-N=128000 # Check time N=2k -> N=4k => time -> < 4*time
+N=8000 # Check time N=2k -> N=4k => time -> < 4*time
 i=0
 r=[]
 while i< N:
@@ -93,9 +109,11 @@ while i< N:
         r += [ sc * random() ]
         i += 1
 """
+
 print("Sorting "+str(r) )
-print( Qsort(r) )
+Qsort(r)
 print("Original array now: "+str(r) )
 """
 Qsort(r)
+"""
 
